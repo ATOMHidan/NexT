28b98acb200d2c76b1dd31e013af651616bfbab8
ATOMHidan <2628606624@qq.com>
2026-09-15 22:47:33 +0800

    Use slim top loading bar and disable background request restarts

--- js/third-party/pace.js
+++ js/third-party/pace.js
@@ -1,6 +1,8 @@
 /* global Pace */
 
 Pace.options.restartOnPushState = false;
+// Keep background requests from restarting the page-loading indicator.
+Pace.options.restartOnRequestAfter = false;
 
 document.addEventListener('pjax:send', () => {
   Pace.restart();
